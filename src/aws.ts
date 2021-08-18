6e3de39b3b95a47735281c6221861d8e2b8451fd
trucopa <roqejr1307@gmail.com>
2021-08-17 21:32:07 -0300

    changing aws keys

--- src/aws.ts
+++ src/aws.ts
@@ -2,9 +2,9 @@ import 'dotenv/config'
 import aws from 'aws-sdk'
 
 aws.config.update({
-    secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY,
-    accessKeyId:process.env.AWS_ACCESS_KEY_ID,
-    region:process.env.AWS_REGION
+    secretAccessKey:'sTkDvCnHbnaHlx2nw9vLHSj0ZAmLhjxwKBKRK2Ue',
+    accessKeyId:'AKIAUXUKQPZO4GH45HJS',
+    region:'us-east-2'
 });
 
 const s3 = new aws.S3();
